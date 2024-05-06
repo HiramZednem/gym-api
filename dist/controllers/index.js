78cb76bd7d73c130f104ed37cf5e47b58a1c380b
Hiram Mendez <hirammendez000@gmail.com>
2024-05-05 21:09:51 -0600

    node_modules deleted

--- dist/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.gymController = void 0;
-var gym_controller_1 = require("./gym.controller");
-Object.defineProperty(exports, "gymController", { enumerable: true, get: function () { return gym_controller_1.gymController; } });
